8dc502f58cae9216bdc359356df3b39570e5d5a4
Max Cantor <max@maxcantor.net>
2024-05-16 21:16:37 -0500

    Add some more student files.

---
+++ ka.py
@@ -0,0 +1,4 @@
+MY_NAME ="Karwan"
+MY_MEDIA = [" Love in Bland (Series in NETFLEX)", "Grit (book)"]
+def mutate(string):
+    return string + " I love Sandwitch"
---
+++ kh.py
@@ -0,0 +1,5 @@
+MY_NAME = "kashma"
+MY_MEDIA = ["the lion kng (movie)","invisible cities (book)"]
+
+def mutate(string):
+    return string + "I Love Pizza"
